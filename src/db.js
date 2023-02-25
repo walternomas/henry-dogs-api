fa77d532b150622aabe8f08e2074d0ea4ace1f10
walternomas <w.barriosvazquez@gmail.com>
2023-02-25 07:48:55 -0300

    change db connection

--- src/db.js
+++ src/db.js
@@ -3,11 +3,12 @@ const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE
+  PGUSER, PGPASSWORD, PGHOST, /*PGPORT,*/ PGDATABASE
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${ PGDATABASE }`, {
-//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+// const sequelize = new Sequelize(`postgres://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${ PGDATABASE }`, {
+const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`, {
+  //const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
